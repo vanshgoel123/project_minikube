fd4e3e59a9f3c4c8d3ca6fe4ad2808c56467628e
vanshgoel123 <vanshg247776@gmail.com>
2026-02-25 15:15:52 +0530

    deployed

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Hello from DevOps Demo!",
+        "message": "Hello",
         "version": os.getenv("APP_VERSION")
     })
 
