196031b86c45741046f5c6a606a675dafeeef967
vanshgoel123 <vanshg247776@gmail.com>
2026-02-25 14:21:46 +0530

    check

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Hello from DevOps ci/cd pipeline ! ",
+        "message": "Hello from DevOps ",
         "version": os.getenv("APP_VERSION", "v1")
     })
 
